42b763912e09f881987b4f8aa9cab518a415342b
Rafael Cardona <rcardonasanta@gmail.com>
2025-07-23 12:35:17 +0000

    domain-name-generator-rafael

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
+  
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+    const pronouns = ['the', 'our','these','those'];
+    const adjs = ['great', 'big','small','nice'];
+    const nouns = ['jogger', 'racoon','shirt','hat'];
+    const domains = ['.com', '.net', '.org', '.es'];
+
+    const resultado = document.getElementById('resultado');
+
+    let html = '';
+
+    for (let pronoun of pronouns) {
+      for (let adj of adjs) {
+        for (let noun of nouns) {
+          for (let domain of domains) {
+            const namedomain = `${pronoun}${adj}${noun}${domain}`;
+            html += `<li>${namedomain}</li>`;
+          }
+        }
+      }
+    }
+
+    resultado.innerHTML = html;
+
+    console.log("muestra mis namedominios correctamnte");
+  };
